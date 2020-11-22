3700a1ee6a8168bb8c6ffd9bfe1d1ae708fc61f5
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2020-11-22 12:37:36 +0200

    update

--- hw6-redux/src/components/product-list-by-category/ProductListByCategory.js
+++ hw6-redux/src/components/product-list-by-category/ProductListByCategory.js
@@ -1,17 +1,38 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 
-import {fetchProductsByCategory} from "../../redux/actions";
+import {fetchProductsByCategory, itemInCartToggle, itemInWishListToggle} from "../../redux/actions";
 import {ProductItem} from "./product-item/ProductItem";
 
 export const ProductListByCategory = ({match: {params: {nameOfCategory}}}) => {
 
     const products = useSelector(({productsByCategoryReducer: {products}}) => products);
+    const cart = useSelector(({cartReducer: {cart}}) => cart);
+    const wishList = useSelector(({wishListReducer: {wishList}}) => wishList);
     const dispatch = useDispatch();
 
     useEffect(() => {
         fetchProductsByCategory(dispatch, nameOfCategory);
     }, [nameOfCategory]);
 
-    return <> { products.map(product => <ProductItem key={product.id} product={product}/>) } </>;
+    const itemInCart = (product) => {
+        dispatch(itemInCartToggle(product));
+    };
+
+    const itemInWishList = (product) => {
+        dispatch(itemInWishListToggle(product));
+    };
+
+    return(
+        <>
+            {
+                products.map(product => <ProductItem key={product.id}
+                                                     product={product}
+                                                     isItemInCart={cart.find(item => item.id === product.id)}
+                                                     isItemInWishList={wishList.find(item => item.id === product.id)}
+                                                     itemInCart={itemInCart}
+                                                     itemInWishList={itemInWishList} />)
+            }
+        </>
+    );
 };
--- hw6-redux/src/components/product-list-by-category/product-item/ProductItem.js
+++ hw6-redux/src/components/product-list-by-category/product-item/ProductItem.js
@@ -1,36 +1,26 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
 
-import {itemInCartToggle, itemInWishListToggle} from "../../../redux/actions";
 import s from './ProductItem.module.css';
 
-export const ProductItem = ({product}) => {
+export const ProductItem = ({product, isItemInCart, isItemInWishList, itemInCart, itemInWishList}) => {
 
-    const {id, title, price, description, image} = product;
-    const cart = useSelector(({cartReducer: {cart}}) => cart);
-    const wishList = useSelector(({wishListReducer: {wishList}}) => wishList);
-    const dispatch = useDispatch();
+    const {title, price, description, image} = product;
 
-    const isItemInCartWishList = () => {
-        const isItemInCart = cart.find(item => item.id === id);
-        const isItemInWishList = wishList.find(item => item.id === id);
+    const isItemInCartWishList = () => (
+        <>
+            <span className={`material-icons ${ isItemInCart ? s.removeFromCart : s.addToCart }`}
+                  onClick={() => itemInCart(product)}
+                  title={ isItemInCart ? 'remove from cart' : 'add to cart' }>
+                { isItemInCart ? 'remove_shopping_cart' : 'add_shopping_cart' }
+            </span>
 
-        return (
-            <>
-                <span className={`material-icons ${ isItemInCart ? s.removeFromCart : s.addToCart }`}
-                      onClick={() => dispatch(itemInCartToggle(product))}
-                      title={ isItemInCart ? 'remove from cart' : 'add to cart' }>
-                    { isItemInCart ? 'remove_shopping_cart' : 'add_shopping_cart' }
-                </span>
-
-                <span className={`material-icons ${s.favorite}`}
-                      onClick={() => dispatch(itemInWishListToggle(product))}
-                      title={ isItemInWishList ? 'remove from wish list' : 'add to wish list' }>
-                    { isItemInWishList ? 'favorite' : 'favorite_border' }
-                </span>
-            </>
-        );
-    };
+            <span className={`material-icons ${s.favorite}`}
+                  onClick={() => itemInWishList(product)}
+                  title={ isItemInWishList ? 'remove from wish list' : 'add to wish list' }>
+                { isItemInWishList ? 'favorite' : 'favorite_border' }
+            </span>
+        </>
+    );
 
     return (
         <div className={s.productView}>
